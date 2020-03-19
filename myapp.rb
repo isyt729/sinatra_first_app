af239bf68c0fdbd8b8ca098af252e3b9a24abf2f
isyt729 <nwb0807@icloud.com>
2020-03-19 12:21:05 +0900

    first

---
+++ myapp.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter:  "mysql2",
+  host:     "localhost",
+  username: "root",
+  password: "",
+  database: "sinatra_practice",
+  socket: "/tmp/mysql.sock"
+)
+
+class Comment < ActiveRecord::Base
+end
+ 
+get "/" do 
+  @comments = Comment.all
+  erb :index
+end
+
+post "/new" do 
+  Comment.create({body: params[:body]})
+  redirect '/'
+end
+
+post'/delete' do
+  Comment.find(params[:id]).destroy
+end
